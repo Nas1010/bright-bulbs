5471f0f008b1a9436d2c0ac22c4c5fbf55c57254
Nas1010 <184663878+Nas1010@users.noreply.github.com>
2025-07-11 20:03:25 +0100

    add Head component with logo and social media icons layout

---
+++ src/components/common/heading/Head.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Head = () => {
+  return (
+    <div>
+          <section className='head'>
+              <div className='container flexSB'>
+                  <div className='logo'>
+                      <h1>Academia</h1>
+                      <span>ONLINE EDUCATION AND LEARNING</span>
+                  </div>
+                  <div className='social'>
+                      <i className='fab fa-facebook-f'></i>
+                      <i className='fab fa-instagram'></i>
+                  </div>
+              </div>   
+      </section>
+    </div>
+  )
+}
+
+export default Head
