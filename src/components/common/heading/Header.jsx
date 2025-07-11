200a6effd530752ae136a82f54d33082a6a459a3
Nas1010 <184663878+Nas1010@users.noreply.github.com>
2025-07-11 20:40:23 +0100

    add multiple navigation links to Header component for site sections

--- src/components/common/heading/Header.jsx
+++ src/components/common/heading/Header.jsx
@@ -1,11 +1,27 @@
 import React from 'react'
 import Head from './Head.jsx'
 import './header.css'
+import { Link } from 'react-router-dom'
 
 const Header = () => {
   return (
     <div>
-      <Head/>
+      <Head />
+      <header>
+        <nav className='flexSB'>
+          <ul>
+            <li>
+              <Link to="/"> Home</Link>
+              <Link to="/courses"> Courses</Link>
+              <Link to="/about"> About</Link>
+              <Link to="/team"> Team</Link>
+              <Link to="/pricing"> Pricing</Link>
+              <Link to="/journal"> Journal</Link>
+              <Link to= "/contact"> Contact</Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
     </div>
   )
 }
