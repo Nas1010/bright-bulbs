6ac84c443e16d4258b32407eb6bbb27f274a870f
Nas1010 <184663878+Nas1010@users.noreply.github.com>
2025-07-12 14:45:02 +0100

    create initial files for Hero and Home components

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React from 'react'
 import Header from './components/common/heading/Header.jsx'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import './App.css'
+import Home from './components/home/Home.jsx'
 
 const App = () => {
   return (
     <Router>
       <Header />
       <Routes>
-        {/* <Route path="/about" element={<About />} /> */}
+        <Route path="/" element={<Home/>} />
       </Routes>
     </Router>
   )
