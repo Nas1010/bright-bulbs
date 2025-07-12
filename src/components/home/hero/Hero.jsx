efd511392d07ad38a990ddc0c5cd74e6af3cc2c7
Nas1010 <184663878+Nas1010@users.noreply.github.com>
2025-07-12 15:58:32 +0100

    add button and heading styling

--- src/components/home/hero/Hero.jsx
+++ src/components/home/hero/Hero.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import './hero.css'
+import Title from '../../common/title/Title.jsx'
 
 const Hero = () => {
   return (
@@ -7,12 +8,13 @@ const Hero = () => {
           <section className="hero">
           <div className="container">
                   <div className="row">
+                      <Title subtitle="WELCOME TO BRIGHT BULBS" title="Best Online Education"/>
                       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco</p>
                       <div className="button">
                           <button className="primary-btn">
                               GET STARTED NOW <i className="fa fa-long-arrow-alt-right"></i>
                           </button>
-                          <button className="primary-btn">
+                          <button>
                               VIEW COURSES <i className="fa fa-long-arrow-alt-right"></i>
                           </button>
                       </div>
