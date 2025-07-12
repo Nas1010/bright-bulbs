b4a5ce4e3a89e4c8d40c581cb2980c2264b61d67
Nas1010 <184663878+Nas1010@users.noreply.github.com>
2025-07-12 14:58:32 +0100

    build basic hero section

--- src/components/home/hero/Hero.jsx
+++ src/components/home/hero/Hero.jsx
@@ -4,7 +4,23 @@ import './hero.css'
 const Hero = () => {
   return (
     <>
-     <h1>Hero</h1>
+          <section className="hero">
+          <div className="container">
+                  <div className="row">
+                      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco</p>
+                      <div className="button">
+                          <button className="primary-btn">
+                              GET STARTED NOW <i className="fa fa-long-arrow-alt-right"></i>
+                          </button>
+                          <button className="primary-btn">
+                              VIEW COURSES <i className="fa fa-long-arrow-alt-right"></i>
+                          </button>
+                      </div>
+                 </div>
+          </div>
+          </section>
+          <div className="margin"></div>
+          
     </>
   )
 }
