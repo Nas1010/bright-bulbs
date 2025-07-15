e6fd35e79297d7ee64300b972c3f81c305eaf76b
Nas1010 <184663878+Nas1010@users.noreply.github.com>
2025-07-15 22:45:43 +0100

    add AboutCard component to display online learning benefits using homeABout data(dummyData.js)

--- src/components/about/AboutCard.jsx
+++ src/components/about/AboutCard.jsx
@@ -1,23 +1,37 @@
-import React from 'react'
-import Title from '../common/title/Title.jsx'
+import React from "react"
+import Title from "../common/title/Title.jsx"
+import {homeAbout} from "../../dummyData.js"
 
 const AboutCard = () => {
-    return (
-      <>
-            <section className='aboutHome'>
-                <div className='container flexSB'>
-                    <div className='left row'>
-                        <img src="./images/about.webp" alt="About Us"/>
-                    </div>
-                    <div className='right row'>
-                        <Title subtitle='LEARN ANYTHING' title='Benefits About Online Learning' />
-                        <div className='items'>
-                            {homeABout}
-                        </div>
-                    </div>
+  return (
+    <>
+      <section className="aboutHome">
+        <div className="container flexSB">
+          <div className="left row">
+            <img src="./images/about.webp" alt="About Us" />
+          </div>
+          <div className="right row">
+            <Title
+              subtitle="LEARN ANYTHING"
+              title="Benefits About Online Learning"
+            />
+            <div className="items">
+              {homeAbout.map((val) => (
+                <div className="items flexSB">
+                  <div className="img">
+                    <img src={val.cover} alt={val.title} />
+                  </div>
+                  <div className="text">
+                    <h2>{val.title}</h2>
+                    <p>{val.desc}</p>
+                  </div>
                 </div>
-             </section>
-      </>
+              ))}
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
   )
 }
 
