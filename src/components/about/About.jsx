2bfeffa4eb15ae181f8540bab5d11bcc0a81206a
Nas1010 <184663878+Nas1010@users.noreply.github.com>
2025-07-15 22:13:49 +0100

    add styling for back component

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import Back from '../common/back/Back.jsx'
 
 const About = () => {
     return (
       <>
-        <h1>About</h1>         
+            <h1>About</h1>    
+        <Back title =  'About us'/>    
       </>
    
   )
