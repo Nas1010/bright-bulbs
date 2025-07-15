d73c3c9d32c121603d1392e2d724094bc9414cb4
Nas1010 <184663878+Nas1010@users.noreply.github.com>
2025-07-15 21:59:52 +0100

    create about route in App.jsx

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import Header from './components/common/heading/Header.jsx'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import './App.css'
 import Home from './components/home/Home.jsx'
+import About from './components/about/About.jsx'
 
 const App = () => {
   return (
     <Router>
       <Header />
       <Routes>
-        <Route path="/" element={<Home/>} />
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About/>} />
       </Routes>
     </Router>
   )
